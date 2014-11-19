a188c2b23cbe11071e769f61232453d2eb7b04c0
ewittig <elizabeth@puppetlabs.com>
2014-11-19 13:46:18 -0800

    adding in noop flag to each notify

--- manifests/compliance.pp
+++ manifests/compliance.pp
@@ -2,18 +2,26 @@ class gpo::compliance {
   include gpo
   
   if $::logoff != 1 {
-    notify { "Out of compliance: Logoff": }
+    notify { "Out of compliance: Logoff": 
+      noop => true,
+    }
   }
 
   if $::lockoutbadcount > 10 {
-    notify { "Out of compliance: LockoutBadCount": }
+    notify { "Out of compliance: LockoutBadCount": 
+      noop => true,
+    }
   }
 
   if $::lockoutduration < 10 {
-    notify { "Out of compliance: LockoutDuration": }
+    notify { "Out of compliance: LockoutDuration": 
+      noop => true,
+    }
   }
 
   if $::resetlockoutcount < 10 {
-    notify { "Out of compliance: ResetLockoutCount": }
+    notify { "Out of compliance: ResetLockoutCount": 
+      noop => true,
+    }
   }
 }
